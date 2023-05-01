57f16a80746ac4816b36319bf84b512468f74260
dbs271 <djaudwls7873@gmail.com>
2023-05-01 21:50:39 +0900

    react-toastify 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const notify = (event) => {
+    event.preventDefault(); // 폼 제출 동작 막기
+    toast("toastify test!");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <button onClick={notify}>Click Me</button>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home 화면</h1>
+    </div>
+  );
+};
+export default Home;
+
+// import React from "react";
+
+// class Home extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Home 화면</h1>
+//       </div>
+//     );
+//   }
+// }
+// export default Home;
